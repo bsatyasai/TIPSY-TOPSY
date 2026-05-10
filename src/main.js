90bfc1489b04a31ed95863644daa26246e25c0b7
bsatyasai <ms.satyasai@gmail.com>
2026-05-10 17:48:03 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+document.getElementById('root').innerHTML = `
+  <div style="
+    font-family: Arial;
+    padding: 40px;
+    text-align: center;
+  ">
+    <h1>🔥 TIPSY-TOPSY</h1>
+    <p>Your fashion website is now live on Netlify!</p>
+  </div>
+`;
